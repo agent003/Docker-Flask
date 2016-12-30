f5e3c6c027991f4a5578b24b073144e4d9754177
EKANSH <singhekansh22@gmail.com>
2016-12-30 11:07:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'First Flask app inside container'
+    return 'Flask app inside container scheduled for autmated build. sounds overwhelming.'
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
